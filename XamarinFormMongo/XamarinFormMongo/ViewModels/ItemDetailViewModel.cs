688e931d52c27a15a40b9942c7047cac502cb17e
Alper Dağlı <alper379@gmail.com>
2019-03-22 14:42:23 +0300

    Add project files.

---
+++ XamarinFormMongo/XamarinFormMongo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinFormMongo.Models;
+
+namespace XamarinFormMongo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
